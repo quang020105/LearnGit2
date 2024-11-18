3362821198f735c8eb8c8a2d09bd1862796fb9eb
quang020105 <quangx2qn@gmail.com>
2024-11-18 15:49:45 +0700

    Update equals

--- app/src/main/java/com/example/learngit/Student.kt
+++ app/src/main/java/com/example/learngit/Student.kt
@@ -7,4 +7,23 @@ data class Student (
     fun getAge():Int{
         return age
     }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Student
+
+        if (name != other.name) return false
+        if (age != other.age) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = name.hashCode()
+        result = 31 * result + age
+        return result
+    }
+
 }
